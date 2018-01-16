5f1da7b532014f04a870e2bfff7264b1352bc10d
Nicole W <nicolemairead4@gmail.com>
2018-01-16 18:52:47 -0500

    rspec gig

--- spec/models/gig_spec.rb
+++ spec/models/gig_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Gig, type: :model do
   it 'validates all attributes' do
-    gig = Gig.new(:cost => 15, :start_time => (Time.now.to_i), :end_time => (Time.now + 1.hours).to_i, :name => "Date Night", :description => "Bedtimes at 8pm, try not to give too many snacks!", :parent_id => 1, :sitter_id => 1)
-    expect(gig).to have_attributes(:cost => 15, :start_time => (Time.now.to_i), :end_time => (Time.now + 1.hours).to_i, :name => "Date Night", :description => "Bedtimes at 8pm, try not to give too many snacks!", :parent_id => 1, :sitter_id => 1)
+    gig = Gig.new(:cost => 15, :start_time => nil, :end_time => nil, :name => "Date Night", :description => "Bedtimes at 8pm, try not to give too many snacks!", :parent_id => 1, :sitter_id => 1)
+    expect(gig).to have_attributes(:cost => 15, :start_time => nil, :end_time => nil, :name => "Date Night", :description => "Bedtimes at 8pm, try not to give too many snacks!", :parent_id => 1, :sitter_id => 1)
   end
 
   it 'is not valid without cost' do
@@ -23,6 +23,18 @@ RSpec.describe Gig, type: :model do
     gig.start_time = Time.now
     gig.end_time = Time.now + 1.hours
     expect(gig.start_time).not_to be(gig.end_time)
+  end
+
+  it 'has a string as a name' do
+    gig = Gig.new
+    gig.name = "Running Errands"
+    expect(gig.name).to be_a(String)
+  end
+
+  it 'has a string as a description' do
+    gig = Gig.new
+    gig.description = "Treat yourself to anything in the fridge! There will be pizza for the kids for dinner."
+    expect(gig.description).to be_a(String)
   end
 
 end
