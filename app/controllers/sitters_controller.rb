6147ac33e5dbf793f03d1b1f90c583503b6ca359
Nicole W <nicolemairead4@gmail.com>
2018-02-10 15:16:18 -0500

    add gigs to sitter profile

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -9,6 +9,7 @@ class SittersController < ApplicationController
   def show
     @sitter = Sitter.find(params[:id])
     @requests = Request.all.where(sitter_email: @sitter.email).order('start_time ASC').where("start_time >= ?", Time.now - 24.hours)
+    @gigs = Gig.all.where(sitter_id: current_sitter.id);
   end
 
   private
